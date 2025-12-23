ff0678034a67fd15f66cc6e91e592322f8610111
myatsu.nyein@igt-towers.com <myatsu.nyein@igt-towers.com>
2025-12-23 16:01:45 +0630

    Update module info

--- igt_excel_import/__manifest__.py
+++ igt_excel_import/__manifest__.py
@@ -3,6 +3,12 @@
     'version': '14.0.1.1.0',
     'category': 'Tools',
     'summary': 'Import lines from Excel into models (wizard + configs)',
+    'description': """
+      <p>This module allows importing Excel files into Odoo.</p>
+      <ul>
+      <li>Import line items from Excel</li> 
+      </ul>
+     """,
     'author': 'IGT',
     'depends': ['base'],
     'data': [
@@ -10,7 +16,10 @@
         'views/igt_excel_import_views.xml',
         'wizards/igt_excel_import_wizard.xml'
     ],
+    'images': ['static/description/icon.png','static/description/cover.png'],
     'external_dependencies': {'python': ['openpyxl']},
     'installable': True,
     'application': False,
+    'license': 'LGPL-3',
+    'website': 'https://github.com/MyatSN/igt_excel_import',
 }
